bba7aa27fe94a52069b88db6c97c7ddc424555f8
Swamiraj <swamirajpatilshambhu789@gmail.com>
2025-10-03 21:59:00 +0530

    main

--- High lvl/Medicine Manager(HOLD)/app/app/api/login/route.js
+++
@@ -1,8 +0,0 @@
-export async function POST(request) {
-  const data = await request.json();
-  // You can add your login logic here
-  return new Response(JSON.stringify({ message: "Login received", data }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
